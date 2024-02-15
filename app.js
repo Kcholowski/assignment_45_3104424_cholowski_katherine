f41f3dc0580a93a9604753c01c3ee63f61642d15
Kcholowski <cholowskik@mymacewan.ca>
2024-02-14 17:21:41 -0700

    changing route

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/", indexRouter);
+app.use("/monsters", indexRouter);
 app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
